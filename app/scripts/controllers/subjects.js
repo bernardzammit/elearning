0cee731e7fc831639f7d50db96e7c1f56f026442
natashanikolic <natashanikolic@users.noreply.github.com>
2016-05-07 21:00:32 +0200

    adding subjects per grade when changed adding gamification to science topics in grade 6 adding topic view and controller 3 new states for activities

--- app/scripts/controllers/subjects.js
+++ app/scripts/controllers/subjects.js
@@ -8,10 +8,54 @@
  * Controller of yapp
  */
 angular.module('yapp')
-  .controller('SubjectsCtrl', ['$scope', '$state', function($scope, $state) {
+  .controller('SubjectsCtrl', ['$scope', '$state', 'sharedProperties', function($scope, $state, sharedProperties) {
 
-    $scope.topics = [
-      { subject : 'maths', title: 'Fractions', link: '../weather/story.html'},
+    $scope.selectedYear = sharedProperties.getYear();
+
+    $scope.grade4Topics = [
+      { subject : 'maths', title: '?????'},
+      { subject : 'maths', title: 'Perimeter and area'},
+      { subject : 'maths', title: 'Telling the time'},
+      { subject : 'english', title: 'Poem Writing'},
+      { subject : 'english', title: 'Spelling'},
+      { subject : 'english', title: 'Story telling'},
+      { subject : 'maltese', title: ''},
+      { subject : 'maltese', title: ''},
+      { subject : 'maltese', title: ''},
+      { subject : 'science', title: 'Forces'},
+      { subject : 'science', title: 'Weather Equipment'},
+      { subject : 'science', title: 'Healthy Diets'},
+      { subject : 'ict', title: 'Internet Safety'},
+      { subject : 'ict', title: 'Programming with Logo'},
+      { subject : 'ict', title: 'Using Microsoft Word'},
+      { subject : 'geography', title: 'Farming'},
+      { subject : 'geography', title: 'Reuse, Reduce, Recycle'},
+      { subject : 'geography', title: 'Mediterranean Islands'}
+    ];
+
+    $scope.grade5Topics = [
+      { subject : 'maths', title: '########'},
+      { subject : 'maths', title: 'Perimeter and area'},
+      { subject : 'maths', title: 'Telling the time'},
+      { subject : 'english', title: 'Poem Writing'},
+      { subject : 'english', title: 'Spelling'},
+      { subject : 'english', title: 'Story telling'},
+      { subject : 'maltese', title: ''},
+      { subject : 'maltese', title: ''},
+      { subject : 'maltese', title: ''},
+      { subject : 'science', title: 'Forces'},
+      { subject : 'science', title: 'Weather Equipment'},
+      { subject : 'science', title: 'Healthy Diets'},
+      { subject : 'ict', title: 'Internet Safety'},
+      { subject : 'ict', title: 'Programming with Logo'},
+      { subject : 'ict', title: 'Using Microsoft Word'},
+      { subject : 'geography', title: 'Farming'},
+      { subject : 'geography', title: 'Reuse, Reduce, Recycle'},
+      { subject : 'geography', title: 'Mediterranean Islands'}
+    ];
+
+    $scope.grade6Topics = [
+      { subject : 'maths', title: 'Fractions'},
       { subject : 'maths', title: 'Perimeter and area'},
       { subject : 'maths', title: 'Telling the time'},
       { subject : 'english', title: 'Essay Writing'},
@@ -25,17 +69,39 @@ angular.module('yapp')
       { subject : 'science', title: 'Healthy Diets'},
       { subject : 'ict', title: 'Internet Safety'},
       { subject : 'ict', title: 'Programming with Logo'},
-      { subject : 'ict', title: 'Using Word'},
+      { subject : 'ict', title: 'Using Microsoft Word'},
       { subject : 'geography', title: 'Farming'},
       { subject : 'geography', title: 'Reuse, Reduce, Recycle'},
       { subject : 'geography', title: 'Mediterranean Islands'}
 
     ];
 
+    $scope.getTopics = function () {
+      if ($scope.selectedYear == '4'){
+        return $scope.grade4Topics;
+      }
+      if ($scope.selectedYear == '5') {
+        return $scope.grade5Topics;
+      }
+      else if ($scope.selectedYear =='6'){
+        return $scope.grade6Topics;
+      }
+    }
+
 
     $scope.loadPartial = function(link) {
-      $state.go('topics');
-      //$scope.currentPartial = link;
-      //alert($scope.currentPartial);
+      if (link == 'Forces')
+      {
+        $state.go('forces');
+      }
+      else if (link === 'Weather Equipment')
+      {
+        $state.go('weatherEquipment');
+      }
+      else if (link === 'Healthy Diets')
+      {
+        $state.go('healthyDiets');
+      }
+
     }
   }]);
